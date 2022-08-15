796e9e7b4081fc7f4a684d931d4a8c48a0f7634b
Cesar Wong <cewong@redhat.com>
2022-08-15 12:29:25 -0400

    Introduce v1beta1 API version
    Adds code to convert from v1alpha1 to v1beta1
    Updates code to use the new version

--- support/config/deployment.go
+++ support/config/deployment.go
@@ -1,7 +1,9 @@
 package config
 
 import (
-	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
+	"strings"
+
+	hyperv1 "github.com/openshift/hypershift/api/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
